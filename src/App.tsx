41fc9875af9ec113d27d06b8a2dfa26c52a771ae
Vahan Sahakyan <vahan.sahakyan1996@gmail.com>
2023-01-02 13:54:21 +0400

    added data.utils.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { ChangeEvent, Component, useEffect, useState } from 'react';
 import { CardList } from './components/card-list/card-list.component.jsx';
+import { getData } from './utils/data.utils';
 import SearchBox from './components/search-box/search-box.component';
 // import ReactDOM from "react-dom";
 // import logo from "./logo.svg";
 import './App.css';
 
+type Monster = {
+  id: string;
+  name: string;
+  email: string;
+};
+
 const App = () => {
   const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(users => setMonsters(users));
+    const fetchUsers = async () => {
+      const users = await getData<Array<Monster>>(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setMonsters(users);
+    };
+    fetchUsers();
   }, []);
   useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monster: any) => {
+    const newFilteredMonsters = monsters.filter(monster => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
     setFilteredMonsters(newFilteredMonsters);
   }, [monsters, searchField]);
 
-  const onSearchChange = (event: any) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
