d9ccf4c19022d0699f05235a03c73052b288f6aa
Vahan Sahakyan <vahan.sahakyan1996@gmail.com>
2023-01-02 12:09:23 +0400

    latest update commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component.jsx";
-import { SearchBox } from "./components/search-box/search-box.component.jsx";
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component.jsx';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 // import ReactDOM from "react-dom";
 // import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -11,24 +11,23 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: "",
+      searchField: '',
     };
   }
-
   async componentDidMount() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await res.json();
     console.log(users);
     this.setState({ monsters: users });
   }
 
-  handleChange = (e) => {
+  onSearchChange = e => {
     this.setState({ searchField: e.target.value });
   };
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) =>
+    const { monsters, searchField, title } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     console.log(filteredMonsters);
@@ -37,7 +36,7 @@ class App extends Component {
         <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={this.handleChange}
+          onSearchChange={this.onSearchChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./search-box.styles.css";
+import React from 'react';
+import './search-box.styles.css';
 
-export const SearchBox = ({ placeholder, handleChange }) => (
+export const SearchBox = ({ placeholder, onSearchChange }) => (
   <input
     className="search"
     type="search"
     placeholder={placeholder}
-    onChange={handleChange}
+    onChange={onSearchChange}
   />
 );
